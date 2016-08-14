f4f5371a7b53bbb91ed4fbdf1e13908fd6196f07
Darah <darahfumadac@gmail.com>
2016-08-14 22:38:08 +0800

    Checkin login screen

---
+++ ExamMaker/ExamMaker/Views/LoginScreen.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ExamMaker.Views
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.errorMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernameTxt
+            // 
+            this.usernameTxt.Location = new System.Drawing.Point(78, 12);
+            this.usernameTxt.Name = "usernameTxt";
+            this.usernameTxt.Size = new System.Drawing.Size(135, 20);
+            this.usernameTxt.TabIndex = 0;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(78, 38);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(135, 20);
+            this.passwordTxt.TabIndex = 9;
+            this.passwordTxt.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Password";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(47, 78);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 12;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Location = new System.Drawing.Point(128, 78);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 13;
+            this.cancelBtn.Text = "Exit";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // errorMsg
+            // 
+            this.errorMsg.AutoSize = true;
+            this.errorMsg.ForeColor = System.Drawing.Color.Red;
+            this.errorMsg.Location = new System.Drawing.Point(89, 62);
+            this.errorMsg.Name = "errorMsg";
+            this.errorMsg.Size = new System.Drawing.Size(64, 13);
+            this.errorMsg.TabIndex = 14;
+            this.errorMsg.Text = "Login Failed";
+            this.errorMsg.Visible = false;
+            // 
+            // LoginScreen
+            // 
+            this.AcceptButton = this.loginBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelBtn;
+            this.ClientSize = new System.Drawing.Size(241, 113);
+            this.Controls.Add(this.errorMsg);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.usernameTxt);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginScreen";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTxt;
+        private System.Windows.Forms.MaskedTextBox passwordTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Label errorMsg;
+    }
+}
---
+++ ExamMaker/ExamMaker/Views/LoginScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExamMaker.BusinessObjects;
+using ExamMaker.Models.Models;
+using ExamMaker.Models.Repositories;
+
+namespace ExamMaker.Views
+{
+    public partial class LoginScreen : Form
+    {
+        private AppRepository _appRepository;
+        private LoginManager _loginManager;
+        private User _authenticatedUser;
+
+        public LoginScreen(AppRepository appRepository)
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+
+            _appRepository = appRepository;
+            _loginManager = new LoginManager(appRepository.UserRepository);
+            _authenticatedUser = new User();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            _authenticatedUser = _loginManager.Authenticate(usernameTxt.Text, passwordTxt.Text);
+
+            if (_authenticatedUser.UserId > 0)
+            {
+                Program.LoggedInUser = _authenticatedUser;
+                
+                errorMsg.Visible = false;
+
+                MainMenuScreen mainMenuScreen = new MainMenuScreen(_appRepository, this);
+                mainMenuScreen.Show();
+                Hide();
+            }
+            else
+            {
+                errorMsg.Visible = true;
+            }
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            usernameTxt.Clear();
+            passwordTxt.Clear();
+            
+            usernameTxt.Focus();
+
+            base.OnVisibleChanged(e);
+        }
+    }
+}
